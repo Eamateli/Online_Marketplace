bd3b7b7f1999f4abf2f1b86699116fb8289ba52d
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-02-15 23:33:48 +0200

    Signing up: Debugging forms.py

--- core/forms.py
+++ core/forms.py
@@ -5,5 +5,5 @@ from django.contrib.auth.models import User
 class SignupForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'passowrd2')
+        fields = ('username', 'email', 'password1', 'password2')
         
