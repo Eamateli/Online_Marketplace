92556a0ca4b1f4cd350d7f5b1a986a99e498bbab
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-02-19 20:48:37 +0200

    Adding Items: Debugging the path in the urls.py

--- item/urls.py
+++ item/urls.py
@@ -7,5 +7,5 @@ app_name = 'item'
 
 urlpatterns = [
     path('<int:pk>/', views.details, name='detail'),
-    path('new/', views.detail, name='detail'),
+    path('new/', views.details, name='detail'),
 ]
