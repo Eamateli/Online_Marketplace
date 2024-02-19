b58dda6384dcb1939b5acf2a020170f1d647f38b
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-02-19 20:17:50 +0200

    Adding Items: Adding model and fields

--- forms.py
+++ forms.py
@@ -4,4 +4,5 @@ from item.models import Item
 
 class NewItemForm(forms.ModelForm):
     class Meta:
-        
+        model = Item
+        fields = ('category', 'name', 'description','price','image')
