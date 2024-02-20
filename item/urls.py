8275d42d45cd890cd7eb3bfc26d05fae0f801231
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-02-20 15:53:26 +0200

    Edit: Adding the path

--- item/urls.py
+++ item/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path('new/', views.new, name='new'),
     path('<int:pk>/', views.details, name='details'),
     path('<int:pk>/delete', views.delete, name='delete'),
+    path('<int:pk>/edit', views.edit, name='edit'),
+
        
 ]
