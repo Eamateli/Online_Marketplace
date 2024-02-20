8f86d74e7e20ee8eb4feca3555e2ca0007172db5
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-02-20 14:30:53 +0200

    Trying to modify the styiling

--- item/urls.py
+++ item/urls.py
@@ -6,6 +6,6 @@ from . import views
 app_name = 'item'
 
 urlpatterns = [
-    path('<int:pk>/', views.details, name='detail'),
+    path('<int:pk>/', views.details, name='details'),
     path('new/', views.new, name='new'),
 ]
