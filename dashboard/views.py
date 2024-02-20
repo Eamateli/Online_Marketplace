5ab27857777acdfe34b60daf069b1d5dffe50cb4
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-02-20 14:59:07 +0200

    Dashboard: Adding the index function

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 from item.models import Item
 
 
+@login_required
+def index(request):
+    items = Item.objects.filter(created_by=request.user)
+    
+    return render(request, 'dashboard/index.html', {
+        'items':items,
+    })
