75e8e06e857d53b880d1a8445e9cd6d51cd8980d
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-02-22 19:17:23 +0200

    Communication: Creating the conversation app and adding it to the settings

--- bazaar/settings.py
+++ bazaar/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'conversation',
     'core',
     'item',
     'dashboard',
+    
 ]
 
 MIDDLEWARE = [
